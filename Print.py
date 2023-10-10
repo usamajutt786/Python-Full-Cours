7457530c57ff06bd01e00510498167723f366b0d
Usama <2021se41@student.uet.edu.pk>
2023-10-11 03:16:15 +0500

    Python Data Types

--- Print.py
+++ Print.py
@@ -4,3 +4,6 @@ print("hello","janum","kia","hall","han");
 print("hello","janum","kia","hall","han",sep="/");
 print("hello","janum","kia","hall","han",end="\n");
 print(False);
+
+
+#pyhton data types 
