aab1e8fc71765d4d4accc4a66e9540a730248b2b
Usama <2021se41@student.uet.edu.pk>
2023-10-11 03:09:37 +0500

    Python Print Fucntion Details

---
+++ Print.py
@@ -0,0 +1,6 @@
+# Today we will discuss Print function
+print("Hello");
+print("hello","janum","kia","hall","han");
+print("hello","janum","kia","hall","han",sep="/");
+print("hello","janum","kia","hall","han",end="\n");
+print(False);
