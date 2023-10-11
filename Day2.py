849c0b2d69bdce7cd9a8071ab0cb0ee8ad04c87e
Usama <2021se41@student.uet.edu.pk>
2023-10-11 23:31:01 +0500

    while loop

--- Day2.py
+++ Day2.py
@@ -3,8 +3,23 @@ number=input("Enter a number: ")
 print(number)
 a=input("Enter First Number:")
 b=input("Enter Second Number: ")
-result=int(a)+int(b);
-print(result);
+result=int(a)+int(b)
+print(result)
 print("The Result of Two Numbers  is",result)
 
 list("hello")
+#intendation  a tab space 
+if(a==4):
+    print(" a is 4")
+else:
+        print(" a is not 4")
+
+#while loop in python
+num=int(input("How many time you want to print hello:"))
+i=0
+while i<num:
+      print("Hello")
+      i+=1
+
+
+
