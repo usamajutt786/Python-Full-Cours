c97f04add367bdb26df64df3c4c2b30a9dac64cc
Usama <2021se41@student.uet.edu.pk>
2023-10-12 00:26:28 +0500

    Guessing Game

--- Day2.py
+++ Day2.py
@@ -1,25 +1,39 @@
-#input vaidation in python
-number=input("Enter a number: ")
-print(number)
-a=input("Enter First Number:")
-b=input("Enter Second Number: ")
-result=int(a)+int(b)
-print(result)
-print("The Result of Two Numbers  is",result)
+import random
+ #input vaidation in python
+# number=input("Enter a number: ")
+# print(number)
+# a=input("Enter First Number:")
+# b=input("Enter Second Number: ")
+# result=int(a)+int(b)
+# print(result)
+# print("The Result of Two Numbers  is",result)
 
-list("hello")
-#intendation  a tab space 
-if(a==4):
-    print(" a is 4")
-else:
-        print(" a is not 4")
+# list("hello")
+# #intendation  a tab space 
+# if(a==4):
+#     print(" a is 4")
+# else:
+#         print(" a is not 4")
 
-#while loop in python
-num=int(input("How many time you want to print hello:"))
-i=0
-while i<num:
-      print("Hello")
-      i+=1
+# #while loop in python
+# num=int(input("How many time you want to print hello:"))
+# i=0
+# while i<num:
+#       print("Hello")
+#       i+=1
 
+#Gueesing Number...........
+counter=0
+num=random.randint(1,100);
+GuessedNum=int(input("Enter the number you wanna Guess:"))
+while GuessedNum!=num:
+    counter+=1
+    if GuessedNum<num:
+        print("Guess Higher")
+    else:
+        print("guess lower")
+    GuessedNum=int(input("Enter the number you wanna Guess:"))
+
+print("You Complete it in ",counter, "Attemps")
 
 
