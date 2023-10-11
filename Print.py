581a1f505f4f8348eac9a9bfe9b79809e9c50dbf
Usama <2021se41@student.uet.edu.pk>
2023-10-11 09:01:48 +0500

    Data Types

--- Print.py
+++ Print.py
@@ -7,3 +7,13 @@ print(False);
 
 
 #pyhton data types 
+#Build it or basic data types  e.g int flot double etc 
+#container type data types   e.g  list ,dictionary, set, touple 
+#user define data types   e.g class
+print(34);
+print(True);
+print(False);
+print(23.3);
+print([12,23,34,45,6])  #list
+print({"Name":"Usama","Age":23,"Class":" SE"})   #Dictionary
+print((12,23,433,4,5,3,"Touple"));  #set
