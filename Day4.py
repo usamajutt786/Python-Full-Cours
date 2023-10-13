b5711d2be9bb0d2f395eb51f27ca41536b141eb8
Usama <2021se41@student.uet.edu.pk>
2023-10-13 19:15:38 +0500

    day4 complete

---
+++ Day4.py
@@ -0,0 +1,13 @@
+#in thsis day we will learn about modules in poython
+
+import math #import module math 
+import time #import time library 
+import os
+
+print(math.sqrt(100))
+print(time.ctime())
+print("hello")
+time.sleep(2)
+print("World")
+os.getcwd()
+os.listdir()
