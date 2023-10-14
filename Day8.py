859f64462a939d4175315b78e0b53d20472eb6ae
Usama <2021se41@student.uet.edu.pk>
2023-10-15 01:28:45 +0500

    DAy 8 and 9

---
+++ Day8.py
@@ -0,0 +1,16 @@
+#sets and Dictionary 
+#Sets are immutable non indeing data type 
+
+s={23,34,2,2,3,}
+print(type(s))
+print(s)
+s.remove(34)
+print(s)
+
+
+#Dictionary 
+
+D={"Name":"Usama","Age":23}
+print(type(D))
+print(D)
+print(D.keys)
---
+++ Day9.py
@@ -0,0 +1,18 @@
+#aliasing
+#Mean when we assign value of one variable to another variable it will point the same memory 
+#location  when we delete the variable the value remain exist because deletion just cancel the 
+#references 
+a=5
+b=a
+c=a
+print(id(a))
+print(id(b))
+print(id(c))
+del a
+print(id(b))
+
+x="Corona"
+y=x
+z=y
+#print(sys.getrefcount(a))
+#SystemExit
